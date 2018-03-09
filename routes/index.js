6436f574dc3acab50b0e542e77dc476da47c0c9a
sajirK <h-sk.Sajir@hotmail.fr>
2018-03-09 10:29:48 +0100

    push

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,7 @@ router.post('/upload', function(req, res) {
   let sampleFile = req.files.sampleFile;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('./public/images/'+'avatar'+'.jpg', function(err) {
+  sampleFile.mv('./public/images/'+'sampleFile'+'.jpg', function(err) {
     if (err)
       return res.status(500).send(err);
 console.log('test test 123');
